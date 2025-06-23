561204b869101f7b6abcbc3e0b171ae44cb01ed5
Davar <3dsensing@Davar-2.local>
2025-06-23 16:53:03 -0500

    first non working

--- model_conversion.py
+++ model_conversion.py
@@ -2,24 +2,6 @@ import coremltools as ct
 import torch
 from depth_anything_v2.dpt import DepthAnythingV2
 
-def load_convert_save(outPath):
-
-    torch_model = torchvision.models.mobilenet_v2()
-    torch_model.eval() 
-
-    example_input = torch.rand(1, 3, 256, 256)
-    traced_model = torch.jit.trace(torch_model, example_input)
-
-    # Convert using the same API. Note that we need to provide "inputs" for pytorch conversion.
-    convertedModel = ct.convert(traced_model,
-                        convert_to="mlprogram",
-                        compute_units=ct.ComputeUnit.ALL,
-                        compute_precision=ct.precision.FLOAT16,
-                        inputs=[ct.TensorType(name="input", shape=example_input.shape)])
-
-    convertedModel.save(outPath)
-
-
 def loadTorchModel(modelPath, encoder):
     model_configs = {
         'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
@@ -39,13 +21,19 @@ if __name__ == '__main__':
     torch_model.eval()
 
     #==================== handle input and trace the model
-    step = 14
-    hm = 37   # min 518
-    wm = 50
-    height = hm * step
-    width = wm * step
-    chansels = 3
-    example_input = torch.rand(1, chansels, height, width)
+    channels = 3
+    example_input = torch.rand(1, channels, 518, 518)
     traced_model = torch.jit.trace(torch_model, example_input)
-    
+    shp = (1, channels, ct.RangeDim(lower_bound=518, upper_bound=1988), ct.RangeDim(lower_bound=518, upper_bound=1988)) 
+    input_shape = ct.Shape(shape=shp)
+
     #==================== convert the model
+
+    mlProg = ct.convert(traced_model,
+                        convert_to="mlprogram",
+                        compute_units=ct.ComputeUnit.ALL,          # CPU, GPU, Neural Engine
+                        compute_precision=ct.precision.FLOAT16,    # not only supported by CPU and GPU, but also by Neural Engine
+                        inputs=[ct.TensorType(name="image", shape=input_shape)],
+                        outputs=[ct.TensorType(name="depth")])
+    
+    mlProg.save(f'checkpoints/custom_{encoder}_F16.mlpackage')
