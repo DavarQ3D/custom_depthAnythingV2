817f85fb0d8fd303237d599fb907779b82d4cabc
Davar <3dsensing@Mac.attwifi.manager>
2025-06-26 18:37:35 -0500

    inference from both done

--- compare.py
+++ compare.py
@@ -56,11 +56,25 @@ def center_crop_or_pad(img: np.ndarray, desiredRow: int, desiredCol: int) -> np.
 
 #=============================================================================================================
 
+def denormalize(image):
+    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+    image = image.astype(np.uint8)
+    return image
+
+#=============================================================================================================
+
 def inferFromTorch(model, image, input_size):
     depth = model.infer_image(image, input_size)
-    depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-    depth = depth.astype(np.uint8)
-    return depth
+    return denormalize(depth)
+
+#=============================================================================================================
+
+def inferFromCoreml(mlProg, bgr):
+    rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+    pil_input = Image.fromarray(rgb)
+    pred = mlProg.predict({"image": pil_input})
+    depth = np.array(pred["depth"], dtype=np.float32)
+    return denormalize(depth)
 
 #=============================================================================================================
 
@@ -72,7 +86,7 @@ if __name__ == '__main__':
 
     #------------------ load the Core ML model
     customModel = True
-    model = ct.models.MLModel("./checkpoints/custom_vits_F16.mlpackage") if customModel else ct.models.MLModel("./checkpoints/DepthAnythingV2SmallF16.mlpackage")
+    mlProgram = ct.models.MLModel("./checkpoints/custom_vits_F16.mlpackage") if customModel else ct.models.MLModel("./checkpoints/DepthAnythingV2SmallF16.mlpackage")
 
     #------------------ resizer
     lower_dim = 518
@@ -108,28 +122,5 @@ if __name__ == '__main__':
         resized = sample["image"]                
 
         cropped = center_crop_or_pad(resized, fixedRow, fixedCol)  
-
         depth_torch = inferFromTorch(torch_model, cropped, fixedRow)
-
-
-
-    # #------------------ load the Core ML model
-    
-    # imagePath = "data/camera"
-
-    # bgr = cv2.imread("data/camera/camera_0.png", cv2.IMREAD_COLOR)
-    # rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-
-    # sz = (686, 518) if customModel else (518, 392)
-    # rgb_resized = cv2.resize(rgb, sz, interpolation=cv2.INTER_AREA)
-
-    # pil_input = Image.fromarray(rgb_resized)
-    # pred = model.predict({"image": pil_input})
-
-    # depth = np.array(pred["depth"], dtype=np.float32)
-    # norm = cv2.normalize(depth, None, 0, 255, cv2.NORM_MINMAX)
-    # cv2.imshow("Depth", norm.astype(np.uint8))
-    # key = cv2.waitKey(0)
-    # if key == 27:  
-    #     cv2.destroyAllWindows()
-    #     exit()
+        depth_coreml = inferFromCoreml(mlProgram, cropped)
