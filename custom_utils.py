fixing correct masking

custom_utils.py
@@ -298,8 +298,10 @@ def weightedLeastSquared(pred, gt, guessInitPrms, k_lo=0.5, k_hi=3, num_iters=5,
 
 #=============================================================================================================
 
-def getValidDataMask(image):
-    return (image > 0) & np.isfinite(image)
+def getValidMaskAndClipExtremes(image, minVal, maxVal):
+    mask = np.isfinite(image) & (image > minVal) & (image < maxVal) 
+    image = np.clip(image, minVal, maxVal)  
+    return mask, image
 
 #=============================================================================================================
 
@@ -340,7 +342,5 @@ def handlePredictionSteps(raw_image, gt, makeSquareInput, borderType, useCoreML,
         
         pred    = cv2.resize(pred,    (gt.shape[1], gt.shape[0]), interpolation=cv2.INTER_CUBIC)
         cropped = cv2.resize(cropped, (gt.shape[1], gt.shape[0]), interpolation=cv2.INTER_CUBIC)
-
-    pred = np.maximum(pred, 1e-2)     # ensure no negative values
 
     return pred, gt, cropped